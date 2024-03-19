770f49a15fb754413181c0fc5029983224eaacd5
TillKuck <tilloberto@gmail.com>
2024-03-19 21:25:35 +0100

    Finishing model class

--- source/model.py
+++ source/model.py
@@ -8,17 +8,55 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 from tqdm import tqdm
 
 class XGBoostModel:
+    """
+    A class for building, training, and evaluating an XGBoost classifier model.
+    -------------
+    Attributes:
+    - pipeline: The pipeline used for preprocessing the data.
+    - model: The trained XGBoost classifier model.
+    - param_grid: The hyperparameter grid for tuning the model.
+    """
     def __init__(self):
+        """
+        Initializes the XGBoostModel class.
+        """
         self.pipeline = None
         self.model = None
-        self.param_grid = {}
+        self.param_grid = {
+            'classifier__n_estimators': [100, 200, 300], # [100, 200, 300]
+            'classifier__max_depth': [3, 5, 7], # [3, 5, 7]
+            'classifier__learning_rate': [0.1, 0.01, 0.001], # [0.1, 0.01, 0.001]
+            'classifier__objective': ['binary:logistic']
+        }
 
     def split_train_test(self, data, target_col, test_size=0.25, random_state=42):
+        """
+        Splits data into training and testing sets.
+        -------------
+        Parameters:
+        - data: The DataFrame containing the data.
+        - target_column: The name of the target column.
+        -------------
+        Returns:
+        - X_train: The feature matrix of the training data.
+        - X_test: The feature matrix of the testing data.
+        - y_train: The target labels of the training data.
+        - y_test: The target labels of the testing data.
+        """
         X = data.drop(target_col, axis=1)
         y = data[target_col]
         return train_test_split(X, y, test_size=test_size, random_state=random_state)
 
     def build_pipeline(self, X):
+        """
+        Builds a preprocessing pipeline based on the data features.
+        -------------
+        Parameters:
+        - X: The feature matrix.
+        -------------
+        Returns:
+        - pipeline: The preprocessing pipeline.
+        """
         # Identify categorical and numerical features
         categorical_features = X.select_dtypes(include=['object']).columns.tolist()
         numerical_features = X.select_dtypes(include=['int64', 'float64']).columns.tolist()
@@ -47,29 +85,26 @@ class XGBoostModel:
         return self.pipeline
 
     def fit_model(self, X_train, y_train):
+        """
+        Fits the XGBoost classifier model to the training data.
+        -------------
+        Parameters:
+        - X_train: The feature matrix of the training data.
+        - y_train: The target labels of the training data.
+        """
         # Calculate scale_pos_weight
         neg_count = np.sum(y_train == 0)
         pos_count = np.sum(y_train == 1)
         scale_pos_weight = round(neg_count / pos_count, 2)
         
-        # Define parameters for hyperparameter tuning
-        self.param_grid = {
-            'classifier__n_estimators': [100, 200, 300],
-            'classifier__max_depth': [3, 5, 7],
-            'classifier__learning_rate': [0.1, 0.01, 0.001],
-            'classifier__min_samples_leaf': [1, 2, 4],
-            'classifier__min_samples_split': [2, 5, 10],
-            'classifier__objective': ['binary:logistic'],
-            'classifier__scale_pos_weight': [scale_pos_weight]
-        }
+        # Update param_grid with scale_pos_weight
+        self.param_grid['classifier__scale_pos_weight'] = [scale_pos_weight]
         
         # Perform grid search with cross-validation
         self.model = GridSearchCV(self.pipeline, self.param_grid, cv=5, verbose=1)
         with tqdm(total=len(self.param_grid['classifier__n_estimators']) *
                         len(self.param_grid['classifier__max_depth']) *
-                        len(self.param_grid['classifier__learning_rate']) *
-                        len(self.param_grid['classifier__min_samples_leaf']) *
-                        len(self.param_grid['classifier__min_samples_split']),
+                        len(self.param_grid['classifier__learning_rate']),
                   desc="Hyperparameter Tuning") as pbar:
             self.model.fit(X_train, y_train)
             pbar.update()
@@ -77,28 +112,43 @@ class XGBoostModel:
         self.model.fit(X_train, y_train)
 
     def predict(self, X_test):
+        """
+        Makes predictions using the trained model on new data.
+        -------------
+        Parameters:
+        - X_test: The feature matrix of the test data.
+        -------------
+        Returns:
+        - y_pred: The predicted target labels.
+        """
         y_pred = self.model.predict(X_test)
         return y_pred
 
     def evaluate(self, y_true, y_pred):
+        """
+        Evaluates the model's performance on test data.
+        -------------
+        Parameters:
+        - X_test: The feature matrix of the test data.
+        - y_test: The target labels of the test data.
+        -------------
+        Returns:
+        - evaluation_metrics: Dictionary containing evaluation metrics.
+        """
         # Calculate evaluation metrics
         accuracy = accuracy_score(y_true, y_pred)
-        precision = precision_score(y_true, y_pred)
-        recall = recall_score(y_true, y_pred)
         f1 = f1_score(y_true, y_pred)
         roc_auc = roc_auc_score(y_true, y_pred)
         conf_matrix = confusion_matrix(y_true, y_pred)
         baseline_accuracy = max(y_true.mean(), 1 - y_true.mean())
 
         # Print evaluation metrics
-        print("Evaluation Metrics:")
+        print("Baseline Accuracy (always predict majority class):", baseline_accuracy)
+        print("\nEvaluation Metrics:")
         print("Accuracy:", accuracy)
-        print("Precision:", precision)
-        print("Recall:", recall)
         print("F1 Score:", f1)
         print("ROC AUC Score:", roc_auc)
         print("\nConfusion Matrix:")
         print(conf_matrix)
-        print("\nBaseline Accuracy (always predict majority class):", baseline_accuracy)
         print("\nClassification Report:")
         print(classification_report(y_true, y_pred))
